afd6790386ec0637694b1526b9e1a6e62c26b862
Lee Kah Seng <yeos_lee@yahoo.com.sg>
2019-12-08 21:04:18 +0800

    Make navigation controller as style observer

---
+++ SwiftSenpai-Observer-In-App-Dark-Mode/MyNavigationController.swift
@@ -0,0 +1,18 @@
+//
+//  MyNavigationController.swift
+//  SwiftSenpai-Observer-In-App-Dark-Mode
+//
+//  Created by Lee Kah Seng on 08/12/2019.
+//  Copyright © 2019 Lee Kah Seng. All rights reserved.
+//
+
+import UIKit
+
+class MyNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        startObserving(&UserInterfaceStyleManager.shared)
+    }
+}
